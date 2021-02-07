9ed10968030f4c8fc4ece65d0da99fbb9fb47461
Kirill <piksarus@gmail.com>
2021-02-07 17:32:51 +0300

    first commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Todo App</h1>
+        <label for="input"> Add todo
+            <input type="text" id="input" v-model="newTodoText"/>
+        </label>
+        <ol>
+            <li v-for="(todo, key) of todos" :key="key" :todo="todo"> {{ todo }} </li>
+        </ol>
+        <button @click="addTodo">Add</button>
+        <button @click="removeTodo">Remove</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Todo",
+        props: ['todo'],
+
+        data() {
+            return {
+                newTodoText: '',
+                todos: [],
+            }
+        },
+
+        methods: {
+            addTodo() {
+              this.todos.push(this.newTodoText);
+            },
+
+            removeTodo() {
+                this.todos.pop()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
